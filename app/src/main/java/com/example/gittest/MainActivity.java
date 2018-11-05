18684d564a07601daa92dda80ad6073bccb6761a
Xing315 <1573388342@qq.com>
2018-11-05 14:49:20 +0800

    git测试

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //手动在github上更改
+        //手动在本地更改
     }
 }
